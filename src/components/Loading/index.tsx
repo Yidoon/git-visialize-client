c6687d1dc41c9126c1d8cbc3446251b0c9811adc
Yidoon <1471797269@qq.com>
2022-05-26 21:50:52 +0800

    feat: optimize loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,28 @@
+import './index.less'
+
 interface ILoading {
   loading: boolean
 }
 const Loading: React.FC<ILoading> = (props) => {
   const { loading } = props
   const renderContent = () => {
-    return loading ? <span>Loading...</span> : props.children
+    return loading ? (
+      <div className="flex">
+        <div className="flex-1 flex justify-center items-center align-center">
+          <div>
+            <img
+              src="public/images/loading.svg"
+              className="h-20 inline-block mr-3 loading-animate"
+            />
+            <p className="mt-2 text-center"> Loading...</p>
+          </div>
+        </div>
+      </div>
+    ) : (
+      props.children
+    )
   }
-  return <div className="loading">{renderContent()}</div>
+  return <div className="loading h-full">{renderContent()}</div>
 }
 
 export default Loading
